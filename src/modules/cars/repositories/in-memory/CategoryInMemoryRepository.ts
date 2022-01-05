6e5654f21382ba1f27ebca1ffb3ae94f995e063e
leonardo.sepulveda <leonardo.sepulveda@precato.com.br>
2022-01-05 09:33:52 -0300

    test: ensure create category will throw with duplicated name

---
+++ src/modules/cars/repositories/in-memory/CategoryInMemoryRepository.ts
@@ -0,0 +1,23 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../interfaces/ICategoriesRepository";
+
+export class CategoriesInMemoryRepository implements ICategoriesRepository {
+  private categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = await this.categories.find((c) => c.name === name);
+    return category;
+  }
+  async list(): Promise<Category[]> {
+    return this.categories;
+  }
+  async create(data: ICreateCategoryDTO): Promise<Category> {
+    const category = new Category();
+    Object.assign(category, { ...data });
+    this.categories.push(category);
+    return category;
+  }
+}
--- src/modules/cars/services/CreateCategoryService.spec.ts
+++ src/modules/cars/services/CreateCategoryService.spec.ts
@@ -1,8 +1,37 @@
-import { Category } from "../entities/Category";
+import { AppError } from "../../../errors/AppError";
+import { CategoriesInMemoryRepository } from "../repositories/in-memory/CategoryInMemoryRepository";
+import { ICategoriesRepository } from "../repositories/interfaces/ICategoriesRepository";
+import { CreateCategoryService } from "./CreateCategoryService";
 
+let createCategoryService: CreateCategoryService;
+let categoriesInMemoryRepository: ICategoriesRepository;
 describe("Create Category", () => {
-  it("should create a new category", () => {
-    const category = new Category();
-    expect(category).toBe(category);
+  beforeEach(() => {
+    categoriesInMemoryRepository = new CategoriesInMemoryRepository();
+    createCategoryService = new CreateCategoryService(
+      categoriesInMemoryRepository
+    );
+  });
+  it("should create a new category", async () => {
+    const name = "Valid_name";
+    const category = await createCategoryService.execute({
+      name,
+      description: "Valid_Description",
+    });
+    const foundCategory = await categoriesInMemoryRepository.findByName(name);
+    expect(category).toBe(foundCategory);
+  });
+  it("should not create a duplicated category", () => {
+    expect(async () => {
+      const name = "Duplicated_name";
+      await createCategoryService.execute({
+        name,
+        description: "Valid_Description",
+      });
+      await createCategoryService.execute({
+        name,
+        description: "Valid_Description",
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
